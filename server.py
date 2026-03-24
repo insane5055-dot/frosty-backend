52a82c8a03768edc5d9b7ef3ac28fddff6b29858
frosty-trader <insane.5055@gmail.com>
2026-03-24 23:34:44 +0530

    Update server.py

--- server.py
+++ server.py
@@ -531,4 +531,4 @@ def history():
 if __name__ == "__main__":
     start_ws_thread()
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
