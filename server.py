f4bd50c15ffc58367180021b1f7b9959196c8001
frosty-trader <insane.5055@gmail.com>
2026-05-07 20:06:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -270,9 +270,9 @@ def search_symbols():
 
         results.append({
             "symbol": r[COL_DISPLAY],
-            "full_name": r[COL_DISPLAY],
+            "full_name": r[COL_EXCHANGE] + ":" + r[COL_DISPLAY],
             "description": r[COL_DISPLAY],
-            "exchange": "",
+            "exchange": r[COL_EXCHANGE],
             "ticker": r[COL_EXCHANGE] + ":" + r[COL_DISPLAY],
             "type": tv_type
         })
