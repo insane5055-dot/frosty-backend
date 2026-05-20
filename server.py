428997777a8b5d67608fb0b15f38f63ad10a2a29
frosty-trader <insane.5055@gmail.com>
2026-05-20 18:58:02 +0530

    Update server.py

--- server.py
+++ server.py
@@ -37,7 +37,8 @@ CORS(
         r"/*": {
             "origins": "*"
         }
-    }
+    },
+    supports_credentials=True
 )
 
 # =========================================================
@@ -49,7 +50,9 @@ socketio = SocketIO(
     cors_allowed_origins="*",
     async_mode="gevent",
     ping_timeout=30,
-    ping_interval=25
+    ping_interval=25,
+    logger=True,
+    engineio_logger=True
 )
 
 # =========================================================
@@ -125,28 +128,16 @@ def load_scrip_master():
         if "INSTRUMENT" in c
     )
 
-    # =====================================================
-    # CLEAN DATA
-    # =====================================================
-
     SCRIP_MASTER.dropna(
         subset=[COL_DISPLAY],
         inplace=True
     )
-
-    # =====================================================
-    # PREPROCESS
-    # =====================================================
 
     SCRIP_MASTER["DISPLAY_UPPER"] = (
         SCRIP_MASTER[COL_DISPLAY]
         .astype(str)
         .str.upper()
     )
-
-    # =====================================================
-    # FAST SEARCH CACHE
-    # =====================================================
 
     for _, row in SCRIP_MASTER.iterrows():
 
@@ -184,6 +175,7 @@ def load_scrip_master():
 # =========================================================
 
 @app.route("/")
+@cross_origin()
 def home():
 
     return "Backend running OK"
@@ -321,15 +313,12 @@ def resolve_symbol():
             exchange_param = ""
             symbol = symbol_raw
 
-        symbol = symbol.replace(" ", "")
-
         df = SCRIP_MASTER.copy()
 
         df["MATCH"] = (
             df[COL_DISPLAY]
             .astype(str)
             .str.upper()
-            .str.replace(" ", "")
         )
 
         row = df[df["MATCH"] == symbol]
@@ -341,6 +330,8 @@ def resolve_symbol():
             ]
 
         if row.empty:
+
+            print("❌ SYMBOL NOT FOUND:", symbol)
 
             return jsonify({
                 "error": "symbol not found"
@@ -807,7 +798,7 @@ def start_dhan_ws():
 
     ws = websocket.WebSocketApp(
 
-        f"wss://api-feed.dhan.co?version=2&token={ACCESS_TOKEN}&clientId=1101310334&authType=2",
+        f"wss://api-feed.dhan.co?version=2&token={ACCESS_TOKEN}&clientId=YOUR_CLIENT_ID&authType=2",
 
         on_message=on_message,
 
